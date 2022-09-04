e0002bef7b55ba06cac686151263da42e4b5fbfd
Yash <garg.yashvardhan@gmail.com>
2022-09-04 12:58:28 +0400

    updated db reference

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from flask import Flask, jsonify, request
-from firebase_admin import credentials, firestore, initialize_app
+from firebase_admin import credentials, firestore, initialize_app, firestore
 
 app = Flask(__name__)
 
@@ -14,6 +14,13 @@ def hello():
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
+todo_ref = db.collection('test')
+
+
+# @app.route('/add', methods=['POST'])
+# def create():
+
+    
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main___':
