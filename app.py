5046662f9ca34638bf69e9ab5e3cdcae416ba2ef
Yash <garg.yashvardhan@gmail.com>
2022-09-04 12:26:55 +0400

    added new

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return('Hello world')
+
 
 # initialise Firebase
 cred = credentials.Certificate('key.json')
@@ -13,4 +17,4 @@ db = firestore.client()
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main___':
-    app.run(threaded=True, host='0.0.0.0', port=port)
+    app.run()
