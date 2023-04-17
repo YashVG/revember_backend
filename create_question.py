4269bca9b8bb8b487d6a45467621def8d58bbe32
Yash <garg.yashvardhan@gmail.com>
2023-04-17 20:45:39 +0400

    fix

--- create_question.py
+++ create_question.py
@@ -45,13 +45,21 @@ def check_length_of_answer_list(answerList):
 def add_questions(finalLstEnts, duplicateText):
     questionLst = []
     count = 0
+    try:
+        for i in duplicateText:
+            for ent in finalLstEnts:
+                if str(ent[0][0]) in i:
+                    question = i.replace(ent[0][0], "_____")
+                    questionLst.append(question)
+    except:
+        for i in finalLstEnts:
+            for ent in i:
+                print(ent)
+                question = duplicateText[count].replace(ent[0], '_____')
+                print(question)
+                questionLst.append(question)
 
-    for i in finalLstEnts:
-        for ent in i:
-            print(ent)
-            question = duplicateText[count].replace(ent[0], '_____')
-            print(question)
-            questionLst.append(question)
+            count += 1
 
     return questionLst
 
